a7b54b78e8e2800c9e12209f4e554b079556cde3
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-03 09:22:19 -0500

    Zo

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router()
 /* READ */
 router.get('/:id', verifyToken, getUser)
 router.get('/:id/friends', verifyToken, getUserFriends)
-router.get('/userName', searchUser)
+router.get('/username', verifyToken, searchUser)
 
 /* UPDATE */
 router.patch('/:id/:friendId', verifyToken, addRemoveFriend)
