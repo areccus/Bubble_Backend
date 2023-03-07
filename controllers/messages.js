1d4883c0a47da5699f5d5cb03b1dd7f0281de907
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-07 08:57:31 -0500

    added more routes

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,8 +1,8 @@
 import User from "../models/User.js"
-import Message from "../models/Message.js";
+import Message from "../models/Message.js"
 
 export const createMessage = async (req, res) => {
-    const {userId, message} = req.body;
+    const {userId, message} = req.body
     const user = await User.findById(userId)
     const newMessage = new Message({
         userId,
@@ -10,58 +10,70 @@ export const createMessage = async (req, res) => {
         recipient: user.userName,
         message: message,
         timestamp: new Date(),
-      });
+      })
 
       newMessage.save((err, savedMessage) => {
         if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'An error occurred while saving the message.' });
+          console.error(err)
+          return res.status(500).json({ error: 'An error occurred while saving the message.' })
         }
     
-        return res.json(savedMessage);
-      });
+        return res.json(savedMessage)
+      })
 }
 
 export const getMessage = async (req, res) => {
-  const { sender, recipient } = req.query;
+  const { sender, recipient } = req.query
 
-  const senderUser = await User.findOne({ userName: sender });
-  const recipientUser = await User.findOne({ userName: recipient });
+  const senderUser = await User.findOne({ userName: sender })
+  const recipientUser = await User.findOne({ userName: recipient })
 
   if (!senderUser || !recipientUser) {
-    return res.status(404).json({ error: "User not found." });
+    return res.status(404).json({ error: "User not found." })
   }
 
   Message.find({ userId: { $in: [senderUser._id, recipientUser._id] } }, (err, messages) => {
     if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "An error occurred while retrieving the messages." });
+      console.error(err)
+      return res.status(500).json({ error: "An error occurred while retrieving the messages." })
     }
 
-    return res.json(messages);
-  });
-};
+    return res.json(messages)
+  })
+}
+
+export const getMessagedUsers = async (req, res) => {
+  try {
+    const { userId } = req.params
+    const messages = await Message.find({ $or: [{ sender: userId }, { recipient: userId }] })
+    const usernames = [...new Set(messages.map((message) => message.sender === userId ? message.recipient : message.sender))]
+    res.json(usernames)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Failed to get messaged users." })
+  }
+}
 
   // Create group chat
   export const createGroupChat = async (memberUsernames, groupName) => {
     try {
       // Find user documents by their usernames
-      const members = await Promise.all(memberUsernames.map(username => User.findOne({ userName: username })));
-      const memberIds = members.map(member => member._id);
+      const members = await Promise.all(memberUsernames.map(username => User.findOne({ userName: username })))
+      const memberIds = members.map(member => member._id)
   
       // Create new group chat
       const newGroupChat = new GroupChat({
         members: memberIds,
         name: groupName,
         messages: []
-      });
+      })
   
-      await newGroupChat.save();
+      await newGroupChat.save()
   
-      return newGroupChat;
+      return newGroupChat
     } catch (error) {
-      console.error(error);
-      throw new Error('An error occurred while creating the group chat.');
+      console.error(error)
+      throw new Error('An error occurred while creating the group chat.')
     }
-  };
+  }
 
