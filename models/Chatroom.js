c1addf8946a196b03fb474b097c2b086cc1a7058
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-08 13:26:22 -0500

    commented out Chatroom model

--- models/Chatroom.js
+++ models/Chatroom.js
@@ -1,47 +1,47 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-const ChatroomSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    members: [
-        {
-            userId: {
-            type: String,
-            required: true
-            },
-            userName: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    messages: [
-        {
-            userId: {
-              type: String,
-              required: true,
-              ref: "User",
-            },
-            sender: {
-              type: String,
-              required: true,
-            },
-            message: {
-              type: String,
-              required: true,
-            },
-            createdAt: {
-              type: Date,
-              default: Date.now,
-            },
-        }
-    ]
-  },
-  { timestamps: true }
-);
+// const ChatroomSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     members: [
+//         {
+//             userId: {
+//             type: String,
+//             required: true
+//             },
+//             userName: {
+//                 type: String,
+//                 required: true
+//             }
+//         }
+//     ],
+//     messages: [
+//         {
+//             userId: {
+//               type: String,
+//               required: true,
+//               ref: "User",
+//             },
+//             sender: {
+//               type: String,
+//               required: true,
+//             },
+//             message: {
+//               type: String,
+//               required: true,
+//             },
+//             createdAt: {
+//               type: Date,
+//               default: Date.now,
+//             },
+//         }
+//     ]
+//   },
+//   { timestamps: true }
+// );
 
-const Chatroom = mongoose.model("Chatroom", ChatroomSchema);
-export default Chatroom
+// const Chatroom = mongoose.model("Chatroom", ChatroomSchema);
+// export default Chatroom
