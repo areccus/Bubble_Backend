83a72c268c167146c6bcd55967899160ca75593a
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-08 09:30:04 -0500

    added :id

--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../middleware/auth.js'
 import {getUser} from '../controllers/users.js'
-import { getMessage, createMessage, getMessagedUsers, createGroupChat } from '../controllers/messages.js'
+import { getMessage, createMessage, getMessagedUsers, createChatroom } from '../controllers/messages.js'
 
 const router = express.Router()
 
 router.get('/', verifyToken)
 
-router.post('/', verifyToken, createGroupChat)
+router.post('/:id', verifyToken, createChatroom)
 
 export default router
