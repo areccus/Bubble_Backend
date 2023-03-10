d846f6576d1652b244c7f9137951dd4758a0be41
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-10 09:57:37 -0500

    fogot to add chat to index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import { register } from './controllers/auth.js'
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
+import chatRoutes from './routes/chat.js'
 import messageRoutes from './routes/messages.js'
 import { verifyToken } from './middleware/auth.js'
 import { createPost} from './controllers/posts.js'
@@ -60,6 +61,7 @@ app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
 app.use('/posts', postRoutes)
 app.use('/messages', messageRoutes)
+app.use('/chat', chatRoutes)
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 6001
