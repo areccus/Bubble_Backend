f194859364bea7497f9502ab8f307064ed4b3bc7
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-11 10:41:54 -0500

    removed sse

--- controllers/chat.js
+++ controllers/chat.js
@@ -60,29 +60,13 @@ export const postMessage = async (req, res) => {
 export const getMessage = async (req, res) => {
     try {
       const { chatId } = req.params;
-  
       const chat = await Chat.findById(chatId);
       if (!chat) {
-        res.status(404).json({ message: "Chat not found" });
+        res.status(404).json({ message: 'Chat not found' });
         return;
       }
-  
-      // set headers for SSE
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Type', 'text/event-stream');
-      res.setHeader('Connection', 'keep-alive');
-      res.flushHeaders();
-  
-      // listen for new messages and send them to the client
-      chat.on('newMessage', (newMessage) => {
-        res.write(`data: ${JSON.stringify(newMessage)}\n\n`);
-      });
-  
-      // remove event listener when the client closes the connection
-      req.on('close', () => {
-        chat.off('newMessage');
-        res.end();
-      });
+      const messages = chat.messages;
+      res.status(200).json(messages);
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
