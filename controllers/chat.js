98195fa0da48f887521173e54f5cc4c8ed428e67
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-10 22:10:43 -0500

    added sse to the backend

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,69 +1,68 @@
 import User from '../models/User.js'
-import Chat
- from '../models/Chat.js'
+import Chat from '../models/Chat.js'
 
- export const createChat = async (req, res) => {
-    try {
-        const { chatId } = req.params
-        const { members } = req.body
-        const newChat = new Chat({
-            chatId,
-            members,
-        })
-        await newChat.save()
-        res.status(201).json(newChat)
-    } catch (err) {
-        res.status(409).json({ message: err.message })
-    }
+export const createChat = async (req, res) => {
+  try {
+    const { chatId } = req.params
+    const { members } = req.body
+    const newChat = new Chat({
+      chatId,
+      members,
+    })
+    await newChat.save()
+    res.status(201).json(newChat)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
 }
 
 export const postMessage = async (req, res) => {
-    try {
-      const { chatId } = req.params;
-      const { userId, message } = req.body;
-  
-      const user = await User.findById(userId);
-      if (!user) {
-        res.status(404).json({ message: "User not found" });
-        return;
-      }
-  
-      const chat = await Chat.findById(chatId);
-      if (!chat) {
-        res.status(404).json({ message: "Chat not found" });
-        return;
-      }
-  
-      const newMessage = {
-        userId: userId,
-        userName: user.userName,
-        message: message,
-        userPicturePath: user.picturePath,
-        sentAt: new Date()
-      };
-  
-      chat.messages.push(newMessage);
-      await chat.save();
-  
-      res.status(201).json(chat);
-    } catch (err) {
-      res.status(409).json({ message: err.message });
+  try {
+    const { chatId } = req.params
+    const { userId, message } = req.body
+
+    const user = await User.findById(userId)
+    if (!user) {
+      res.status(404).json({ message: 'User not found' })
+      return
     }
-  };
-  
-  export const getMessage = async (req, res) => {
-    try {
-        const { chatId } = req.params;
-    
-        const chat = await Chat.findById(chatId);
-        if (!chat) {
-          res.status(404).json({ message: "Chat not found" });
-          return;
-        }
-    
-        const messages = chat.messages;
-        res.status(200).json(messages);
-      } catch (err) {
-        res.status(500).json({ message: err.message });
-      }
+
+    const chat = await Chat.findById(chatId)
+    if (!chat) {
+      res.status(404).json({ message: 'Chat not found' })
+      return
+    }
+
+    const newMessage = {
+      userId: userId,
+      userName: user.userName,
+      message: message,
+      userPicturePath: user.picturePath,
+      sentAt: new Date(),
+    }
+
+    chat.messages.push(newMessage)
+    await chat.save()
+
+    res.status(201).json(chat)
+  } catch (err) {
+    res.status(409).json({ message: err.message })
   }
+}
+
+export const getMessages = async (req, res) => {
+  try {
+    const { chatId } = req.params
+
+    const chat = await Chat.findById(chatId)
+    if (!chat) {
+      res.status(404).json({ message: 'Chat not found' })
+      return
+    }
+
+    const messages = chat.messages
+    res.status(200).json(messages)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
--- routes/chat.js
+++ routes/chat.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../middleware/auth.js'
-import { createChat, postMessage, getMessage } from '../controllers/chat.js'
+import { createChat, postMessage, getMessages } from '../controllers/chat.js'
 
 const router = express.Router()
 
 router.get('/', verifyToken)
 router.post('/:chatId', verifyToken, createChat)
 router.post('/:chatId/messages', verifyToken, postMessage)
-router.get('/:chatId/messages', verifyToken, getMessage)
+router.get('/:chatId/messages', verifyToken, getMessages)
 
 export default router
