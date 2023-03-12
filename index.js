8415311f2603ddb8a5c3361b0a9823a073518357
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-12 19:41:03 -0400

    added fs and changed post route

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const upload = multer({storage})
 
 /* Routes with files */
 app.post('/auth/register', upload.single('picture'), register)
-app.post('/posts', verifyToken, upload.single('picture'), createPost)
+// app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
 /* ROUTES */
 app.use('/auth', authRoutes)
