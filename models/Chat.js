184a6709a2c9ac4245752e5745a80823e4e4667e
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-12 00:45:47 -0500

    adds userName to chatschema

--- models/Chat.js
+++ models/Chat.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose"
 
 const chatSchema = new mongoose.Schema({
     members: [{
-      type: String,
-      required: true
+      userId: {
+        type: String,
+        required: true
+      },
+      userName: {
+        type: String,
+        required: true
+      },
     }],
     messages: [{
       userId: {
