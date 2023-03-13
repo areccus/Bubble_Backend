7574793e3666cded76d772617e1f70af6537e478
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-13 07:56:29 -0400

    added uploads to vercel.json

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 /* CREATE */
 export const createPost = async (req, res) => {
   try {
-    const { userId, description } = req.body
+    const { userId, description, picturePath } = req.body
     const user = await User.findById(userId)
     const newPost = new Post({
       userId,
@@ -14,7 +14,7 @@ export const createPost = async (req, res) => {
       location: user.location,
       description,
       userPicturePath: user.picturePath,
-      picturePath: '/uploads/' + req.file.filename,
+      picturePath,
       likes: {},
       comments: [],
     })
