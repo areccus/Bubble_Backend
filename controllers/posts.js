652af3ecfa5fc0a2348a3aa115a16c84b4d411a5
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2023-03-24 21:01:42 -0400

    should get more details about the 409 error

--- controllers/posts.js
+++ controllers/posts.js
@@ -24,7 +24,7 @@ export const createPost = async (req, res) => {
     const post = await Post.find()
     res.status(201).json(post)
   } catch (err) {
-    res.status(409).json({ message: err.message })
+    res.status(409).json({ message: err.message, stack: err.stack })
   }
 }
 
